8d78838789c755f77397058e7c82891e377136c3
Mingyu Wang <mingyuwang@MingyudeMacBook.local>
2017-11-22 17:06:44 +0000

    Initial Commit

---
+++ final project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  final project
+//
+//  Created by Mingyu Wang on 2017/11/22.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ final project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  final project
+//
+//  Created by Mingyu Wang on 2017/11/22.
+//  Copyright © 2017年 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
